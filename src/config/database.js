762299da877314fd87429ee775c7080994259a28
Ashutoshwizz <ashutoshwizz121@gmail.com>
2026-02-25 00:39:00 +0530

    securing credentials

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,14 @@
 //here i will write the logic to connect to our data base
 
 const mongoose = require('mongoose');
+require('dotenv').config();
 //this is to connect to cluster
 
 const connectdb= async ()=>{
-    await mongoose.connect("mongodb+srv://ashudev:Ashudev@cluster0.cwd6inb.mongodb.net/devtinder");
+   // mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_NAME};
+     await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}`
+    );
 };
 
 
